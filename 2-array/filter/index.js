46bb3be743d311c0f0629c22e3a02c51415db841
imdreamer <743295483@qq.com>
2020-09-21 18:03:10 +0800

    [yangqian] implement array filter.

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,13 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(o => o % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter((item, index, self) => {
+    return self.indexOf(item) === index;
+  });
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
