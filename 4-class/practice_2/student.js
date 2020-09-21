93814e9e3a73d25c9641c266c7854410edf56598
imdreamer <743295483@qq.com>
2020-09-21 19:32:27 +0800

    [yangqian] implement class practice-2.

--- 4-class/practice_2/student.js
+++ 4-class/practice_2/student.js
@@ -1,1 +1,13 @@
 // TODO 11: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, Class) {
+    super(name, age);
+    this.Class = Class;
+  }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.Class}.`;
+  }
+}
