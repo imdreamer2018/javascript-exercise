758029d27b1cde6b5c763bb68ad8fa8486a6fc85
imdreamer <743295483@qq.com>
2020-09-21 20:26:35 +0800

    [yangqian] implement async xhr.

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,11 +2,22 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  xhr.open('GET', url);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+    if (xhr.status === 200) {
+      successCallback(xhr.responseText);
+    } else {
+      errorCallback(xhr.status);
+    }
+  };
+  xhr.send();
   // end -->
 }
 
-const URL = 'http://localhost:3000/api';
+const URL = 'http://localhost:8080/rs/list';
 fetchData(
   URL,
   result => {
