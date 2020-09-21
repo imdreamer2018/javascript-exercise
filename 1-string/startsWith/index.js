ac109ba5d0fb641c6eafcc4fec527e2a2328c873
imdreamer <743295483@qq.com>
2020-09-21 15:52:24 +0800

    [yangqian] implement string test.

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,8 @@
+import _ from 'lodash';
+
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  return _.filter(collection, o => {
+    return o.match('粤A');
+  }).length;
 }
