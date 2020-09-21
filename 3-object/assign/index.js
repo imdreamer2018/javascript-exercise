1c5213d0fe8491c46340846aeb013d9db6c584d4
imdreamer <743295483@qq.com>
2020-09-21 18:57:26 +0800

    [yangqian] implement object assign.

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,12 @@
+import _ from 'lodash';
+
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  return Object.assign(source, {
+    serialNumber: '12345',
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  });
 }
