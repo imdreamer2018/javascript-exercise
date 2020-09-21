18863762d79ffa210d3cf32c59b42bdd009a18dc
imdreamer <743295483@qq.com>
2020-09-21 20:45:50 +0800

    [yangqian] implement async await

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,8 +1,11 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const result = await fetch(url);
+  const data = await result.json();
+  document.writeln(data.message);
   // end -->
 }
 
-const URL = 'http://localhost:3000/api';
+const URL = 'http://localhost:8080/rs/lists';
 fetchData(URL);
