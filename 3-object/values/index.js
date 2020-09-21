7646e3d2cc3c39f80968b9b003000f44337e9b14
imdreamer <743295483@qq.com>
2020-09-21 19:10:36 +0800

    [yangqian] implement object values.

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,8 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  return Object.values(source)
+    .map(data => {
+      return +data;
+    })
+    .reduce((previousValue, currentValue) => previousValue + currentValue);
 }
